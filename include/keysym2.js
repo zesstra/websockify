ac632bc1b20005c33fcf1333b5d6adb9ebc06b40
Sebastian Sester <sesters@informatik.uni-freiburg.de>
2013-07-02 04:19:35 +0200

    Removed unnecessary lines from keysym2.js

--- include/keysym2.js
+++ include/keysym2.js
@@ -3,8 +3,8 @@
  * Copyright (C) 2010 Joel Martin
  * Licensed under LGPL-3 (see LICENSE.txt)
  *
- * Adjusted by XRM@github / YEN@nightfall on 19jun13,
- * so certain keys won't be blocked with the current wstelnet.js
+ * Adjusted by XRM@github / YEN@nightfall on 19jun13
+ * to using onkeydown *and* onkeypressed.
  * 
  */
 
@@ -23,8 +23,6 @@ function getKeysym(e) {
         case 46        : keysym = 0xFFFF; break; // DELETE
         case 36        : keysym = 0xFF50; break; // HOME
         case 35        : keysym = 0xFF57; break; // END
-        case 33        : keysym = 1 /*0xFF55*/; break; // PAGE_UP
-        case 34        : keysym = 1 /*0xFF56*/; break; // PAGE_DOWN
         case 37        : keysym = 0xFF51; break; // LEFT
         case 38        : keysym = 0xFF52; break; // UP
         case 39        : keysym = 0xFF53; break; // RIGHT
@@ -39,15 +37,11 @@ function getKeysym(e) {
         case 119       : keysym = 0xFFC5; break; // F8
         case 120       : keysym = 0xFFC6; break; // F9
         case 121       : keysym = 0xFFC7; break; // F10
-        case 122       : keysym = 1/*0xFFC8*/; break; // F11
-        case 123       : keysym = 1/*0xFFC9*/; break; // F12
         case 16        : keysym = 0xFFE1; break; // SHIFT
         case 17        : keysym = 0xFFE3; break; // CONTROL
-        //case 18        : keysym = 0xFFE7; break; // Left Meta (Mac Option)
         case 18        : keysym = 0xFFE9; break; // Left ALT (Mac Command)
         default        : keysym = evt.keyCode; break;
     } 
-
     return keysym;
 }
 
